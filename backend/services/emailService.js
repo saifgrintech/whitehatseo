fba0d083b600f34702b7115c325cced77fd06888
kumar <kumardevloper22@gmail.com>
2026-04-06 13:19:32 +0530

    email changed

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -16,9 +16,9 @@ const sendEmail = (formData) => {
   // Email notification to you
   const mailOptions = {
     from: process.env.EMAIL_USER,
-    to: 'ankush.grintech@gmail.com', // Your email for notifications
+    // to: 'ankush.grintech@gmail.com', // Your email for notifications
     // to: 'info@whitehatseo.in', 
-    // to: 'rajesh.rbgroup@gmail.com',
+    to: 'rajesh.rbgroup@gmail.com',
     subject: 'New Contact Form Submission on Whitehatseo',
     html: `
     <html>
